809df91cf14b20d8bf4233121d9489b29457f9de
cherifyounouss <chinsanity16@gmail.com>
2021-04-13 18:09:28 +0000

    init

---
+++ ckanext/extrafields/plugin.py
@@ -0,0 +1,40 @@
+import ckan.plugins as p
+import ckan.plugins.toolkit as tk
+
+
+class ExampleIDatasetFormPlugin(p.SingletonPlugin, tk.DefaultDatasetForm):
+	p.implements(p.IDatasetForm)
+	p.implements(p.IConfigurer)
+    
+	# IConfigurer
+
+	def update_config(self, config):
+		tk.add_template_directory(config, 'templates')
+
+	def create_package_schema(self):
+		# let's grab the default schema in our plugin
+		schema = super(ExampleIDatasetFormPlugin, self).create_package_schema()
+		# our custom field
+		schema.update({'custom_text': [tk.get_validator('ignore_missing'), tk.get_converter('convert_to_extras')]})
+		return schema
+
+	def update_package_schema(self):
+		schema = super(ExampleIDatasetFormPlugin, self).update_package_schema()
+		# our custom field
+		schema.update({'custom_text': [tk.get_validator('ignore_missing'), tk.get_converter('convert_to_extras')]})
+		return schema
+
+	def show_package_schema(self):
+		schema = super(ExampleIDatasetFormPlugin, self).show_package_schema()
+		schema.update({'custom_text': [tk.get_converter('convert_from_extras'), tk.get_validator('ignore_missing')]})
+		return schema
+
+	def is_fallback(self):
+		# Return True to register this plugin as the default handler for
+		# package types not handled by any other IDatasetForm plugin.
+		return True
+
+	def package_types(self):
+		# This plugin doesn't handle any special package types, it just
+		# registers itself as the default (above).
+		return []
